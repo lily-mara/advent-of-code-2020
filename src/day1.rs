32e4665d792b849252a6ab988ed55f9884c1b0a2
Nate Mara <natemara@fastmail.com>
2020-12-01 18:58:21 -0800

    Solve day1

--- src/day1.rs
+++ src/day1.rs
@@ -8,15 +8,51 @@ pub fn gather_input(input: &str) -> Vec<i32> {
 
 #[aoc(day1, part1)]
 pub fn part1(input: &Vec<i32>) -> i32 {
-    0
+    for x in input {
+        for y in input {
+            if x + y == 2020 {
+                return x * y;
+            }
+        }
+    }
+
+    panic!("No two numbers summed to 2020")
 }
 
 #[aoc(day1, part2)]
 pub fn part2(input: &Vec<i32>) -> i32 {
-    0
+    for x in input {
+        for y in input {
+            if x + y < 2020 {
+                for z in input {
+                    if x + y + z == 2020 {
+                        return x * y * z;
+                    }
+                }
+            }
+        }
+    }
+
+    panic!("No three numbers summed to 2020")
 }
 
 #[cfg(test)]
 mod tests {
-    // tests!(1, 3408471, 5109803);
+    tests!(1, 793524, 61515678);
+    test_examples!((
+        "1721
+979
+366
+299
+675
+1456
+" =>
+        514579
+    ), ("1721
+979
+366
+299
+675
+1456
+" => 241861950));
 }
